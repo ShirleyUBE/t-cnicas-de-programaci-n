b05e18581f31f572004cba9e5b41f1a64b23fae4
ShirleyUBE <ss.ubes@uea.edu.ec>
2024-06-08 16:47:35 -0500

    version1

---
+++ POO.py
@@ -0,0 +1,8 @@
+# Programación Orientada a Objetos
+
+print("Repositorio de código fuente utilizado en la Asignatura Programación Orientada a Objetos")
+
+print("Institución: Universidad Estatal Amazónica")
+print("Carrera: Ingeniería en Tecnologías de la Información y Comunicación")
+print("Docente: Ing. Edwin Gustavo Fernández Sánchez, Mgs.")
+
